426f4ee8feac3e1971bf2af2b7bf1546815616fa
Murshed Jamil Ahmed <mahmed@panoramaed.com>
2025-10-14 20:30:22 +0000

    move comment

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
-# renovate: datasource=github-releases depName=awslabs/amazon-eks-ami versioning=loose
 variable "eks_ami_alias" {
   type    = string
+  # renovate: datasource=github-releases depName=awslabs/amazon-eks-ami versioning=loose
   default = "al2023@v20250904"
 }
